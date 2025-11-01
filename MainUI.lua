c0134afca28e0a6b83a21db51e9e41e51a1f021b
ATGFAIL <167156538+ATGFAIL@users.noreply.github.com>
2025-11-01 19:26:36 +0700

    Update MainUI.lua

--- MainUI.lua
+++ MainUI.lua
@@ -4378,7 +4378,7 @@ local aa = {
                 ["lucide-x-square"] = "rbxassetid://10747384217",
                 ["lucide-zoom-in"] = "rbxassetid://10747384552",
                 ["lucide-zoom-out"] = "rbxassetid://10747384679",
-                ["lucide-castle"] = "rbxassetid://131308325593492"
+                ["lucide-castle"] = "rbxassetid://89680811679779"
             }
         }
     end,
